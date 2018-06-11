65ae5b90fa56f4aa100d3b82c6cd50e0bfee6f23
Olga Schretter <olga.schretter@tdmsystems.com>
2018-06-11 17:39:35 +0200

    initial project

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('Cosmo', 'Cocktail', 'https://cocktailsworld.net/wp-content/uploads/Cosmopolitan.jpg' ),
+    new Recipe('Caipi', 'Cocktail', 'https://cocktailsworld.net/wp-content/uploads/Caipirinha.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+
+}
